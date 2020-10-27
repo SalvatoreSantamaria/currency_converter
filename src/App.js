8e364a49450ccae2b0bc874dfecbbf3449c89c3c
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-10-27 07:30:11 -0700

    added options to dropdown

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import CurrencyRow from './CurrencyRow'
 
 const BASE_URL = 'https://api.exchangeratesapi.io/latest'
 
 function App() {
+
+  
+  // returns array of options, first option is what state is ie currencyOptions() function, second option is value that allows us to set ie setCurrencyOptions() function
+  const [currencyOptions, setCurrencyOptions] = useState([]) //empty array is default
+  //console.log(currencyOptions) all of the different currency options
+
+  useEffect(() => { 
+    fetch(BASE_URL)
+    .then(response => response.json())
+    .then(data => { //convert data into array of options. Get just the key portion of the rates. Destructure with the ...
+      setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      console.log(data)
+    })
+
+  }, []) //empty array means useEffect will only be called once
   return (
     <> 
     <h1>Convert</h1>
-    <CurrencyRow />
+    <CurrencyRow 
+      currencyOptions={currencyOptions} //pass in currencyOptions as a prop
+    />
     <div className="equals">=</div>
-    <CurrencyRow />
+    <CurrencyRow 
+      currencyOptions={currencyOptions} //pass in currencyOptions as a prop
+    />
     </>
   );
 }
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,11 +1,17 @@
 import React from 'react' 
 
-export default function CurrencyRow() {
+export default function CurrencyRow(props) {
+  // destructure props into objects
+  const {
+    currencyOptions
+  } = props
   return (
     <div>
       <input type="number" className="input"/>
       <select>
-        <option value="Hi">Hi</option>
+        {currencyOptions.map(prop => (
+        <option key={prop} value={prop}>{prop}</option>
+        ))}
       </select>
     </div>
   )
